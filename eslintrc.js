fd53b30e64426ca8e7bd402029bdcacb9fac3aa9
Yongjin Huang <1765941460@qq.com>
2022-05-17 13:55:49 +0800

    fix eslint errors

---
+++ eslintrc.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+module.exports = {
+    root: true,
+    extends: [
+        'eslint:recommended'
+    ],
+    "overrides": [
+        {
+            "files": [
+                "**/*.ts",
+                "**/*.tsx"
+            ],
+            "env": {
+                "browser": true,
+                "es6": true,
+                "node": true
+            },
+            "extends": [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended"
+            ],
+            "globals": {
+                "Atomics": "readonly",
+                "SharedArrayBuffer": "readonly"
+            },
+            "parser": "@typescript-eslint/parser",
+            "parserOptions": {
+                "ecmaFeatures": {
+                    "jsx": true
+                },
+                "ecmaVersion": 2018,
+                "sourceType": "module",
+                "project": "./tsconfig.json"
+            },
+            "plugins": [
+                "@typescript-eslint"
+            ],
+            "rules": {
+                "indent": [
+                    "error",
+                    2,
+                    {
+                        "SwitchCase": 1
+                    }
+                ],
+                "linebreak-style": [
+                    "error",
+                    "unix"
+                ],
+                "quotes": [
+                    "error",
+                    "single"
+                ],
+                "comma-dangle": [
+                    "error",
+                    "always-multiline"
+                ],
+                "@typescript-eslint/no-explicit-any": 0
+            }
+        }
+    ]
+};
